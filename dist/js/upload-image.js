938c317a1fa9347897bd1218a0491ba21a50d37f
Darshan Bajgain <darshanbajgain90@gmail.com>
2023-12-26 19:53:26 +0545

    upload-image section modified

--- dist/js/upload-image.js
+++ dist/js/upload-image.js
@@ -1,9 +1,9 @@
-// upload-video.js
+// upload-image.js
 
 document.addEventListener("DOMContentLoaded", () => {
-    const videoUpload = document.getElementById("video-upload");
-    const videoFilename = document.getElementById("video-filename");
-    const playAndAnalyzeBtn = document.getElementById("play-and-analyze");
+    const videoUpload = document.getElementById("image-upload");
+    const videoFilename = document.getElementById("image-filename");
+    const playAndAnalyzeBtn = document.getElementById("predict");
 
     videoUpload.addEventListener("change", (event) => {
         const file = event.target.files[0];
@@ -14,14 +14,14 @@ document.addEventListener("DOMContentLoaded", () => {
         }
     });
 
-    document.getElementById("video-form").addEventListener("submit", (event) => {
+    document.getElementById("image-form").addEventListener("submit", (event) => {
         event.preventDefault();
         // Assuming an async operation here, like uploading the video
         playAndAnalyzeBtn.textContent = "Uploading...";
         // Here you can implement the logic for uploading the video or any other operation
         setTimeout(() => {
             // After the operation, you might want to reset the button text or show success message
-            playAndAnalyzeBtn.textContent = "Play and Analyze";
+            playAndAnalyzeBtn.textContent = "Predict";
         }, 2000); // Just a placeholder for simulating an operation
     });
 });
